767ce37880bb1235b3684da443f183ae112ec8d3
Steven Kiesewetter <steven@elementxcreative.com>
2023-06-06 09:38:47 -0500

    initial dev commit

---
+++ src/ArticyProject.tsx
@@ -0,0 +1,143 @@
+import _ from "lodash";
+
+class ArticyProject{
+    
+    data:any;
+
+    variables:any;
+    
+    constructor(data:Object){
+        this.data = data;
+        this.variables = {};
+        for (let i = 0; i<this.data.GlobalVariables.length; i++){
+            let namespace = this.data.GlobalVariables[i].Namespace;
+            this.variables[namespace]={};
+            for (let j = 0; j<this.data.GlobalVariables[i].Variables.length; j++){
+                let v = this.data.GlobalVariables[i].Variables[j];
+                this.variables[namespace][v.Variable] = v.Value.toLowerCase();
+            }
+        }
+        console.log("variables initialized",this.variables);
+    }
+
+    GetNodeByID(ID:string){
+        // console.log("Getting node...", ID);
+        for (let i = 0; i < this.data.Packages[0].Models.length; i++) {
+            const element = this.data.Packages[0].Models[i];
+            if (element.Properties == undefined)
+                continue;
+            if (element.Properties.Id == undefined)
+                continue;
+            if (element.Properties.Id == ID)
+                return element;
+        }
+        return undefined;
+    }
+
+    GetStartNode(){
+        for (let i = 0; i < this.data.Packages[0].Models.length; i++) {
+            const element = this.data.Packages[0].Models[i];
+            if (element.Properties == undefined)
+                continue;
+            if (element.Properties.Expression == undefined)
+                continue;
+            if ((element.Properties.Expression as string).includes("HTMLPREVIEW")){
+                return element;
+            }
+        };
+    }
+
+    StoreVariablesFromNode(node:any){
+        if (node.Properties == undefined)
+            return;
+        if (node.Properties.Expression == undefined)
+            return;
+        this.variables = _.merge(this.variables,this.GetVariablesFromNode(node));
+        return undefined;
+    }
+
+    GetVariablesFromNode(node:any){
+       
+        if (node.Properties == undefined)
+            return;
+        if (node.Properties.Expression == undefined)
+            return;
+        
+        var textChunks:string[] = (node.Properties.Expression as string).split("\n");
+
+        var newVars:{[k:string]:any} = {};
+
+        //remove all commented and empty lines
+        for (let i=textChunks.length-1; i>=0; i--){
+            if (textChunks[i].trim().slice(0,2) == "//"){
+                textChunks.splice(i,1);
+                continue;
+            }
+            if (textChunks[i].trim().length > 0){
+                //NEED SANITY CHECKING HERE
+                console.log("processing...",textChunks[i]);
+                let value = this.SplitValueFromText(textChunks[i]);
+                let newData:{[k:string]:any} = {};
+                let dataChunks = this.SplitIndexersFromText(textChunks[i]);
+                newData[dataChunks[dataChunks.length-1].trim()] = value;
+                for (let j=dataChunks.length-2; j>=0; j--){
+                    newData = {[dataChunks[j]]:newData} as {[k:string]:any};
+                }
+                newVars = _.merge(newData,newVars);
+            }
+        }
+        
+        return newVars;
+    }
+
+    CheckConditionString(condition:string){
+        let value = this.SplitValueFromText(condition);
+        let newData:{[k:string]:any} = {};
+        let dataChunks = this.SplitIndexersFromText(condition);
+        newData[dataChunks[dataChunks.length-1].trim()] = value;
+        for (let j=dataChunks.length-2; j>=0; j--){
+            newData = {[dataChunks[j]]:newData} as {[k:string]:any};
+        }
+        console.log(this.variables,newData);
+        return this.ObjectCompare(this.variables,newData);
+    }
+
+    CheckCondition(condition:{}){
+        return this.ObjectCompare(this.variables,condition);
+    }
+
+    ObjectCompare(objx:any,objy:any):boolean{
+        //checks if obj2 key is in obj1
+        //returns value comparison
+
+        function GetValue(obj1:any,obj2:any):any{
+            const obj1Keys = Object.keys(obj1).sort();
+            const obj2Keys = Object.keys(obj2).sort();
+            if (typeof obj1 === 'string'){
+                //we have our value
+                return obj1;
+            }
+            for (let i=0; i<obj2Keys.length; i++){
+                if (obj1Keys.indexOf(obj2Keys[i]) != -1){
+                    let result = GetValue(obj1[obj2Keys[i]],obj2[obj2Keys[i]]);
+                    return result;
+                }
+            }
+            return "false";
+        }
+
+        let obj1Value = GetValue(objx,objy);
+        let obj2Value = GetValue(objy,objy);
+        return obj1Value == obj2Value;
+    }
+
+    SplitIndexersFromText(text:string){
+        return text.split("=")[0].trim().split(".");
+    }
+    SplitValueFromText(text:string){
+        let chunks = text.split("=");
+        return chunks[chunks.length-1].trim().replaceAll(";","").trim();
+    }
+}
+
+export default ArticyProject
---
+++ src/InteractiveArticyViewer.tsx
@@ -0,0 +1,147 @@
+import { useEffect, useState } from "react";
+import QuestionPanel from "./panels/QuestionPanel";
+import ArticyProject from "./ArticyProject";
+import InstructionPanel from "./panels/InstructionPanel";
+
+function InteractiveArticyViewer(){
+
+    const [project, setProject] = useState(undefined as unknown as ArticyProject);
+    const [nodeList, setNodeList] = useState([] as any[])
+
+    var currentNode = nodeList.length>0?nodeList[nodeList.length-1]:undefined;
+    var lastNode = nodeList.length>1?nodeList[nodeList.length-2]:undefined;
+
+    useEffect(()=>{
+        fetch('../Articy Base Project.json?'+(Date.now().toString())).then( response => {return response.json();}).then( data => {
+            setProject(new ArticyProject(data));
+        })
+        .catch((error)=>{
+          console.error(error);
+        });
+    },[]);
+
+    useEffect(()=>{
+        if (project != undefined)
+            setCurrentNode(project.GetStartNode());
+    }, [project]);
+      
+    useEffect(()=>{
+        console.log("currentNode updated:", nodeList[nodeList.length-1]);
+        if (currentNode == undefined)
+            return;
+    }, [nodeList]);
+
+    function setCurrentNode(node:any){
+        // console.log("setting current Node:",node);
+        if (node.Type == "Instruction")
+            project.StoreVariablesFromNode(node);
+        setNodeList([...nodeList,node]);
+    }
+    
+    function DisplayNode(){
+        if (currentNode != undefined){
+            switch (currentNode.Type){
+                case "Instruction":
+                    return (
+                        <InstructionPanel 
+                            text={currentNode.Properties.Expression}
+                            button={{
+                                hidden:false,
+                                text:"Next",
+                                onClick:()=>{
+                                    setCurrentNode(project.GetNodeByID(currentNode.Properties.OutputPins[0].Connections[0].Target));
+                                }
+                            }}
+                        />
+                    )
+                case "DialogueInteractiveFragmentTemplate":
+                    //console.log(currentNode.Properties.OutputPins[0].Connections==undefined);
+                    return (
+                        <InstructionPanel 
+                            text={currentNode.Properties.Text}
+                            button={{
+                                hidden:currentNode.Properties.OutputPins[0].Connections==undefined?true:false,
+                                text:"Next",
+                                onClick:()=>{
+                                    setCurrentNode(project.GetNodeByID(currentNode.Properties.OutputPins[0].Connections[0].Target));
+                                }
+                            }}
+                        />
+                    )
+                case "Hub":
+                    function getConditionText(node:any){
+                        console.log(project.GetNodeByID(node.Target).Properties.Text,project.GetNodeByID(node.Target).Properties.InputPins[0].Text);
+                        return project.GetNodeByID(node.Target).Properties.InputPins[0].Text;
+                    }
+                    var options = currentNode.Properties.OutputPins[0].Connections.map((conn:any)=>(
+                        {
+                            hidden: getConditionText(conn)==""?false:!project.CheckConditionString(getConditionText(conn)),
+                            text:project.GetNodeByID(conn.Target).Properties.Text,
+                            onClick:()=>{
+                                setCurrentNode(project.GetNodeByID(project.GetNodeByID(conn.Target).Properties.OutputPins[0].Connections[0].Target));
+                            }
+                        }));
+
+                    return (
+                        <QuestionPanel 
+                            text={project.GetNodeByID(currentNode.Properties.InputPins[0].Owner).Properties.Text}
+                            buttons={options}
+                        />
+                    )
+                case "Jump":
+                    setTimeout(()=>{
+                        setCurrentNode(project.GetNodeByID(currentNode.Properties.Target));
+                    },0);
+                    return (
+                        <>Please wait...</>
+                    )
+                    break;
+                case "FlowFragment":
+                    setTimeout(()=>{
+                        setCurrentNode(project.GetNodeByID(currentNode.Properties.OutputPins[0].Connections[0].Target));
+                    },0);
+                    return (
+                        <>Please wait...</>
+                    )
+                case "Condition":
+                    let conditions = project.GetVariablesFromNode(currentNode);
+                    let result = false;
+                    if (conditions){
+                        result = project.CheckCondition(conditions);
+                    }
+                    if (result){
+                        setTimeout(()=>{
+                            setCurrentNode(project.GetNodeByID(currentNode.Properties.OutputPins[0].Connections[0].Target));
+                        },0);
+                    }
+                    else{
+                        setTimeout(()=>{
+                            setCurrentNode(project.GetNodeByID(currentNode.Properties.OutputPins[1].Connections[0].Target));
+                        },0);
+                    }
+                    
+                    return (
+                        <>Please wait...</>
+                    )
+                default:
+                    return (
+                        <>
+                        <div>Node of type {currentNode.Type} has not yet been implemented.</div>
+                        </>
+                    )
+            }
+            // return(
+            //     <QuestionPanel text="This is an embedded display." />
+            // )
+        }
+        else return (<>No Current Node</>)
+    }
+
+    return (
+        <div>
+            <DisplayNode />
+        </div>
+    )
+}
+
+export default InteractiveArticyViewer;
